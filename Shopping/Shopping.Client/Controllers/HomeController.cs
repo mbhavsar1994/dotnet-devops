418da3a5aef72ae1030bcca693923e037227e7d9
Mehul Bhavsar <mehul@mehuls-mbp.home>
2024-03-14 14:35:36 -0400

    Intial commit for setting up client app with docker

---
+++ Shopping/Shopping.Client/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Shopping.Client.Data;
+using Shopping.Client.Models;
+
+namespace Shopping.Client.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View(ProductContext.Products);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ Shopping/Shopping.Client/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Shopping.Client.Models
+{
+	public class Product
+	{
+        public string? Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Category { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? ImageFile { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
+
